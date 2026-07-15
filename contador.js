442922aefd1846388cf5acc6976347e326508f52
DilsiaLamadridTorres <lamadrid1992@live.com>
2026-07-15 17:00:01 -0500

    Subiendo contador

--- contador.js
+++ contador.js
@@ -7,6 +7,13 @@ const botonMenos = document.getElementById("disminuir");
 botonMas.addEventListener("click", function () {
     contador = contador + 1;
     numero.textContent = contador;
+
+    if (contador === 10) {
+        numero.style.color = "yellow";
+    }
+    else {
+    numero.style.color = "black"; 
+    }
 });
 
 botonMenos.addEventListener("click", function () {
